e2ad7cc314f57fd94edd96a59a1dd5eafc612999
SuhrobTeshaev <125867321+SuhrobTeshaev@users.noreply.github.com>
2023-10-08 23:52:52 +0700

    new changes

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -7,7 +7,6 @@ const UserItem:FC<UserItemProps> = ({user}) => {
     return ( 
 <div  style={{padding:15,border:'1px solid gray'}}>
                 {user.id},{user.name} проживает в городе {user.address.city} на улице {user.address.street}
-
             </div>
      );
 }
